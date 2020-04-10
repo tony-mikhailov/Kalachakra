1082ce3bcfb74ec1fb8dcd441d01f037402ed510
Tony Mikhailov <tony.mikhailov@gmail.com>
2020-04-10 20:08:38 +0300

    postgres conf

--- kalachakra/kalachakra/settings.py
+++ kalachakra/kalachakra/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'kalachakra.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'vrweb',
+        'USER': 'vrweb_django',
+        'PASSWORD': 'vrweb_django_pass',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation

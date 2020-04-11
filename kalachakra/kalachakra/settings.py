1a290281750c44c7f7c03501cbceb73ec9cca48c
Anton Mikhailov <anton@mikhailov.center>
2020-04-11 11:23:13 +0300

    initial db

--- kalachakra/kalachakra/settings.py
+++ kalachakra/kalachakra/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'kalachakra.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vrweb',
-        'USER': 'vrweb_django',
-        'PASSWORD': 'vrweb_django_pass',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'vrweb',
+#        'USER': 'vrweb_django',
+#        'PASSWORD': 'vrweb_django_pass',
+#        'PORT': '5432',
+#    }
+#}
 
 
 
